b7d5b120dde5750f86a3edf2a5de06a1eb7e7816
QuestCoder72 <tavakkolihg@gmail.com>
2024-10-01 20:51:43 +0330

    Add files via upload

---
+++ Scripts/Traps/EnemyDamage.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyDamage : MonoBehaviour
+{
+    [SerializeField] protected float damage;
+
+    protected void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "Player")
+        {
+            other.GetComponent<Health>().TakeDamage(damage);
+        }
+    }
+}
